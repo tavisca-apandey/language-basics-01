ead6808bf4e5bbcebb7b189fabf8cfde9d216a16
tavisca-apandey <apandey@tavisca.in>
2019-06-24 20:34:06 +0530

    Review Changes

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,6 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-
             //Spliting the Equation.
             String[] terms=equation.Split(new char[]{'*','='});
 
@@ -35,49 +33,48 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 pos=1;
             else
                 pos=2;
-            int a=0,b=0,ccc=0;
+                
+            int firstTerm=0,secondTerm=0,thirdTerm=0;
 
             //Finding the exact missing digit.
             if(pos==0)
             {
-                a=int.Parse(terms[1]);
-                b=int.Parse(terms[2]);
-                double cc=(double)b/a;
-                String c=cc.ToString();
+                bool ifSuccess1=int.TryParse(terms[1], out secondTerm);
+                bool ifSuccess2=int.TryParse(terms[2], out thirdTerm);
+                double tempMissingTerm=(double)thirdTerm/secondTerm;
+                String missingTerm=tempMissingTerm.ToString();
                 //(cc%1==0) to check if the term is decimal; Used mainly for Test 5 type of cases
-                if(c.Length==terms[0].Length&&(cc%1==0))
+                if(missingTerm.Length==terms[0].Length&&(tempMissingTerm%1==0))
                 {
-                    return int.Parse(c[terms[0].IndexOf('?')].ToString());
+                    return int.Parse(missingTerm[terms[0].IndexOf('?')].ToString());
                 }
             }
             else if(pos==1)
             {
-                a=int.Parse(terms[0]);
-                b=int.Parse(terms[2]);
-                double cc=(double)b/a;
-                String c=cc.ToString();
+                bool ifSuccess1=int.TryParse(terms[0], out firstTerm);
+                bool ifSuccess2=int.TryParse(terms[2], out thirdTerm);
+                double tempMissingTerm=(double)thirdTerm/firstTerm;
+                String missingTerm=tempMissingTerm.ToString();
                 //(cc%1==0) to check if the term is decimal; Used mainly for Test 5 type of cases
-                if(c.Length==terms[1].Length&&(cc%1==0))
+                if(missingTerm.Length==terms[1].Length&&(tempMissingTerm%1==0))
                 {
-                    return int.Parse(c[terms[1].IndexOf('?')].ToString());
+                    return int.Parse(missingTerm[terms[1].IndexOf('?')].ToString());
                 }
             }
             else if(pos==2)
             {
-                a=int.Parse(terms[0]);
-                b=int.Parse(terms[1]);
-                ccc=a*b;
-                String c=ccc.ToString();
-                if(c.Length==terms[2].Length)
+                bool ifSuccess1=int.TryParse(terms[0], out firstTerm);
+                bool ifSuccess2=int.TryParse(terms[1], out secondTerm);
+                thirdTerm=firstTerm*secondTerm;
+                String missingTerm=thirdTerm.ToString();
+                if(missingTerm.Length==terms[2].Length)
                 {
-                    return int.Parse(c[terms[2].IndexOf('?')].ToString());
+                    return int.Parse(missingTerm[terms[2].IndexOf('?')].ToString());
                 }
             }
 
             //If none of the above.
             return -1;
-
-            throw new NotImplementedException();
         }
     }
 }
